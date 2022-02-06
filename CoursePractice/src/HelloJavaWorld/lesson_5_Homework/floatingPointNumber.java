076950d03d5f284c90f722e0cbad83fc3b305651
Ion.Gradinari <gradinari.ion@gmail.com>
2022-02-06 23:32:52 +0200

    Lesson 5.3

---
+++ CoursePractice/src/HelloJavaWorld/lesson_5_Homework/floatingPointNumber.java
@@ -0,0 +1,29 @@
+package HelloJavaWorld.lesson_5_Homework;
+
+import java.util.Scanner;
+
+public class floatingPointNumber {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the value: ");
+
+        double npc = input.nextDouble();
+
+        if (npc == 0) {
+            System.out.println("The vallue is zero");
+        } else if (npc < 1 && npc > 0) {
+            System.out.println("The value is small positive");
+        } else if (npc >= 1 && npc <= 1_000_000) {
+            System.out.println("The value is positive");
+        } else if (npc > 1_000_000) {
+            System.out.println("The value is large positive");
+        } else {
+            System.out.println("The value is negative");
+        }
+
+        }
+
+    }
+
