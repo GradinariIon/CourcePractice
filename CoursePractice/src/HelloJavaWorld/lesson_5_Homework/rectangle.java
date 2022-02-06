5bfdd1921c3f67940302e4124df5ef2af2401134
Ion.Gradinari <gradinari.ion@gmail.com>
2022-02-06 19:55:35 +0200

    Lesson 5.2

---
+++ CoursePractice/src/HelloJavaWorld/lesson_5_Homework/rectangle.java
@@ -0,0 +1,39 @@
+package HelloJavaWorld.lesson_5_Homework;
+
+import java.util.Scanner;
+
+public class rectangle {
+
+    public static void main(String[] args) {
+
+        Scanner rectangle1 = new Scanner(System.in);
+
+        System.out.println("The width of rectangle1 is: ");
+        System.out.println("The lenght of rectangle1 is: ");
+
+        int width1 = rectangle1.nextInt();
+        int lenght1 = rectangle1.nextInt();
+
+
+        Scanner rectangle2 = new Scanner(System.in);
+
+        System.out.println("The width of rectangle2 is: ");
+        System.out.println("The lenght of rectangle2 is: ");
+
+        int width2 = rectangle2.nextInt();
+        int lenght2 = rectangle2.nextInt();
+
+        rectangle1.close();
+        rectangle2.close();
+
+        boolean b = ((width1 * lenght1) > (width2 * lenght2));
+        if (b == true)
+            System.out.println("The rectangle1 is bigger");
+        else if (b == false)
+            System.out.println("The rectangle2 is bigger");
+        else
+            System.out.println("The rectangle1 is equal to rectangle2");
+
+    }
+
+}
