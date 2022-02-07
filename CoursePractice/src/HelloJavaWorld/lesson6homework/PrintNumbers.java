879a82a259ddc094a44d2020123745f00b2d5bbe
Ion.Gradinari <gradinari.ion@gmail.com>
2022-02-07 17:37:10 +0200

    Lesson6.3

---
+++ CoursePractice/src/HelloJavaWorld/lesson6homework/PrintNumbers.java
@@ -0,0 +1,12 @@
+package HelloJavaWorld.lesson6homework;
+
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+
+        int numbers = 0;
+        for (int ctr = 1; ctr <= 10; ctr++) {
+            System.out.println(numbers + ctr);
+        }
+    }
+}
